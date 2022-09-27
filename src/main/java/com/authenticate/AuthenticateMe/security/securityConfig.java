19aa880fba2bf0b098fc3f13cd453f083bba36eb
ogunseyebose <aogunseye@ecobank.com>
2022-09-27 14:51:22 +0100

    first

---
+++ src/main/java/com/authenticate/AuthenticateMe/security/securityConfig.java
@@ -0,0 +1,28 @@
+package com.authenticate.AuthenticateMe.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class securityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/register**")
+                .permitAll() .anyRequest().authenticated()
+                .and()
+                .formLogin() .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout() .invalidateHttpSession(true)
+                .clearAuthentication(true) .permitAll();
+    }
+}
---
+++ src/test/java/com/authenticate/AuthenticateMe/AuthenticateMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authenticate.AuthenticateMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticateMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
