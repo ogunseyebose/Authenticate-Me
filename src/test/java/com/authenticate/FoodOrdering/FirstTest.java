c44b1006387fd464b92efadcdbc4ec35eca86a29
ogunseyebose <aogunseye@ecobank.com>
2022-10-28 15:22:54 +0100

    Email Service added

---
+++ src/test/java/com/authenticate/FoodOrdering/FirstTest.java
@@ -0,0 +1,20 @@
+package com.authenticate.FoodOrdering;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class FirstTest {
+
+
+
+        @Test
+    public void testAdd() {
+        assertEquals(90,90);
+
+}
+
+
+
+}
