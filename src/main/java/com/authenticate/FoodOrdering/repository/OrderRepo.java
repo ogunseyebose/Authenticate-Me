af723d5803811ac2d770c81aff26ba21a1744036
ogunseyebose <aogunseye@ecobank.com>
2022-10-28 15:39:18 +0100

    Email Service added

--- src/main/java/com/authenticate/FoodOrdering/repository/OrderRepo.java
+++ src/main/java/com/authenticate/FoodOrdering/repository/OrderRepo.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface OrderRepo extends JpaRepository<Orders, Long> {
     List<Orders> findByUserId(Long userId);
     List<Orders> findByIsCompleted(Character isCompleted);
